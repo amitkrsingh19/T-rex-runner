c92d689f081ee2a32f2237814b4394c389d71efe
Joyprakash Hela <jayhela3@gmail.com>
2026-07-20 17:29:31 +0530

    CHANGES IN AGENT

--- src/rl/agent.py
+++ src/rl/agent.py
@@ -3,7 +3,8 @@ import numpy as np
 import tensorflow as tf
 from model import DQNModel
 from memory import ReplayMemory
-from config import STATE_SHAPE, NUM_ACTIONS, LEARNING_RATE, GAMMA, BATCH_SIZE, REPLAY_BUFFER_SIZE, EPSILON_START, EPSILON_MIN, EPSILON_DECAY
+from config import STATE_SHAPE, NUM_ACTIONS, LEARNING_RATE, GAMMA, BATCH_SIZE, REPLAY_BUFFER_SIZE, EPSILON_START, EPSILON_MIN, EPSILON_DECAY, TARGET_UPDATE_FREQUENCY
+
 
 class DQNAgent:
     def __init__(self, state_shape=STATE_SHAPE, num_actions=NUM_ACTIONS):
@@ -18,13 +19,25 @@ class DQNAgent:
 
         self.batch_size = BATCH_SIZE
 
+        self.target_update_frequency = TARGET_UPDATE_FREQUENCY
+
+        self.training_steps = 0
+
         self.memory = ReplayMemory(REPLAY_BUFFER_SIZE)
 
-        self.model = DQNModel(
+        self.q_network = DQNModel(
             input_shape = self.state_shape,
             num_actions = self.num_actions,
             learning_rate = LEARNING_RATE
         )
+
+        self.target_network = DQNModel(
+            input_shape = self.state_shape,
+            num_actions = self.num_actions,
+            learning_rate = LEARNING_RATE
+        )
+
+        self.update_target_network()
 
     def select_action(self, state):
         if random.random() < self.epsilon:
@@ -32,12 +45,15 @@ class DQNAgent:
         
         state = np.expand_dims(state, axis=0)
         
-        q_values = self.model.model.predict(state, verbose=0)
+        q_values = self.q_network.model.predict(state, verbose=0)
 
         return np.argmax(q_values[0])
     
     def remember(self, state, action, reward, next_state, done):
         self.memory.push(state, action, reward, next_state, done)
+
+    def update_target_network(self):
+        self.target_network.model.set_weights(self.q_network.model.get_weights())
 
     def train(self):
         if len(self.memory) < self.batch_size:
@@ -53,8 +69,8 @@ class DQNAgent:
         rewards = np.array(rewards)
         dones = np.array(dones)
 
-        current_q_values = self.model.model.predict(states, verbose=0)
-        next_q_values = self.model.model.predict(next_states, verbose=0)
+        current_q_values = self.q_network.model.predict(states, verbose=0)
+        next_q_values = self.target_network.model.predict(next_states, verbose=0)
 
         target_q_values = current_q_values.copy()
 
@@ -66,14 +82,23 @@ class DQNAgent:
             
             target_q_values[i][actions[i]] = target
 
-        self.model.model.fit(states, target_q_values, epochs=1, verbose=0)
+        self.q_network.model.fit(states, target_q_values, epochs=1, verbose=0)
+
+        self.training_steps += 1
+
+        if self.training_steps % self.target_update_frequency == 0 :
+            self.update_target_network()
 
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
+            self.epsilon = max(self.epsilon, self.epsilon_min)    
+                
 
     
     def save_model(self, path):
-        self.model.model.save(path)
+        self.q_network.model.save(path)
 
     def load_model(self, path):
-        self.model.model = tf.keras.models.load_model(path)
+        self.q_network.model = tf.keras.models.load_model(path)
+
+        self.update_target_network()

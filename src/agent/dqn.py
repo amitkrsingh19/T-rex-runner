cde91259549d8330ced763f15a752b27f4d29a94
Joyprakash Hela <jayhela3@gmail.com>
2026-08-07 11:32:45 +0530

    Adding DuelingDoubleDQNAgent

--- src/agent/dqn.py
+++ src/agent/dqn.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
 import tensorflow as tf
-from agent.model import DQNModel
+from agent.model import DQNModel, DuelingDQNModel
 from agent.replay_buffer import ReplayMemory
 from config import STATE_SHAPE, NUM_ACTIONS, LEARNING_RATE, GAMMA, BATCH_SIZE, REPLAY_BUFFER_SIZE, EPSILON_START, EPSILON_MIN, EPSILON_DECAY, TARGET_UPDATE_FREQUENCY
 
@@ -218,6 +218,75 @@ class DoubleDQNAgent(BaseAgent):
         return float(loss)
 
 class DuelingDoubleDQNAgent(BaseAgent):
+    def __init__(self, num_actions, gamma=0.99, lr=1e-4):
+        self.state_shape = STATE_SHAPE
+        self.num_actions = num_actions
+        self.gamma = gamma
+
+        self.epsilon_min = EPSILON_MIN
+        self.epsilon_decay = EPSILON_DECAY
+
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
+
+        self.target_update_frequency = TARGET_UPDATE_FREQUENCY
+        self.training_steps = 0
+
+        self.memory = ReplayMemory(REPLAY_BUFFER_SIZE)
+
+        self.q_network = DuelingDQNModel(
+            input_shape = self.state_shape,
+            num_actions = self.num_actions,
+            learning_rate = LEARNING_RATE
+
+        )
+
+        self.target_network = DuelingDQNModel(
+            input_shape = self.state_shape,
+            num_actions = self.num_actions,
+            learning_rate = LEARNING_RATE
+        )
+
+        super().__init__(self.q_network, self.target_network)
+
+    @tf.function
+    def _train_step(self, states, actions, rewards, next_states, dones):
+        dones = tf.cast(dones, tf.float32)
+        rewards = tf.cast(rewards, tf.float32)
+
+        states = states/255.0
+        next_states = next_states/255.0
+
+        next_q_online = self.q_network.model(next_states, training=False)
+
+        best_actions = tf.argmax(next_q_online, axis=1, output_type = tf.int32)
+
+        next_q_target = self.target_network.model(next_states, training=False)
+
+        action_mask = tf.one_hot(best_actions, depth=self.num_actions)
+
+        selected_q = tf.reduce_sum(next_q_target*action_mask, axis=1)
+
+        targets = rewards + (1.0 - dones) * self.gamma * selected_q
+
+        with tf.GradientTape() as tape:
+            current_q_values = self.q_network.model(states, training=True)
+            current_action_mask = tf.one_hot(actions, self.num_actions)
+
+            predicted_q_values = tf.reduce_sum(current_q_values*current_action_mask, axis=1)
+
+            loss = tf.keras.losses.Huber(targets, predicted_q_values)
+
+            gradients = tape.gradient(loss, self.q_network.model.trainable_variables)
+
+            gradients, _ = tf.clip_by_global_norm(gradients, 10.0)
+
+            self.optimizer.apply_gradients(zip(gradients, self.q_network.model.trainable_variables))
+
+            return loss
+
+
+    
+
 
 
 
